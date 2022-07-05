814f5f966a43806a49f14d6c3d794b8c000bb0a0
ciberusps <ciberus.ps@gmail.com>
2022-07-05 09:36:11 +0300

    replace about with FAQ

---
+++ src/renderer/components/FAQ.tsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import { transparentize } from "polished";
+import styled from "styled-components";
+import Button from "./Button";
+
+type TItem = {
+  title: string;
+  description: React.ReactNode;
+};
+
+type TProps = {
+  items: TItem[];
+};
+
+const FAQ = (props: TProps) => {
+  const { items, ...restProps } = props;
+  const [openedItemsTitles, setOpenedItemsTitles] = useState<string[]>([]);
+
+  const toggleItem = (title: string) => {
+    if (openedItemsTitles.includes(title)) {
+      setOpenedItemsTitles(openedItemsTitles.filter((item) => item !== title));
+    } else {
+      setOpenedItemsTitles([...openedItemsTitles, title]);
+    }
+  };
+
+  return (
+    <Container {...restProps}>
+      {items.map((item, index) => {
+        const isOpened = openedItemsTitles.includes(item.title);
+        return (
+          <Item key={index}>
+            <Header>
+              <Title>{item.title}</Title>
+              <ExpandButton
+                icon="chevronUp"
+                size="small"
+                isOpened={isOpened}
+                onClick={() => toggleItem(item.title)}
+              />
+            </Header>
+            <Description isOpened={isOpened}>{item.description}</Description>
+          </Item>
+        );
+      })}
+    </Container>
+  );
+};
+
+export default FAQ;
+
+const Container = styled.div`
+  flex: 1;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  border-radius: 4px;
+  margin-bottom: 100px;
+`;
+
+const Item = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin-top: 10px;
+  background: ${({ theme }) => transparentize(0.92, theme.purple)};
+  border: 1px solid ${({ theme }) => theme.purple};
+  border-radius: 4px;
+`;
+const Header = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  background: ${({ theme }) => transparentize(0.9, theme.purple)};
+  padding: 10px 20px;
+`;
+const Title = styled.div``;
+
+type TDescription = {
+  isOpened: boolean;
+};
+const Description = styled.div<TDescription>`
+  display: ${({ isOpened }) => (isOpened ? "block" : "none")};
+  transition: all 1s ease-out;
+  padding: 10px 20px;
+`;
+
+type TExpandButton = {
+  isOpened: boolean;
+};
+const ExpandButton = styled(Button)<TExpandButton>`
+  transform: ${({ isOpened }) => (isOpened ? "unset" : "rotate(180deg)")};
+`;
--- src/renderer/pages/about.tsx
+++ src/renderer/pages/about.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+import FAQ from "../components/FAQ";
 import Image from "../components/Image";
 import Layout from "../components/Layout";
 
@@ -8,32 +9,73 @@ import addGame2Img from "../../../assets/images/add_game_2.png";
 import settingsImg from "../../../assets/images/settings.png";
 import gameImg from "../../../assets/images/game.png";
 
-const multiplier = 50;
-const width = 16 * multiplier;
-const height = 9 * multiplier;
+const multiplier = 30;
+const SCREENSHOT_WIDTH = 16 * multiplier;
+const SCREENSHOT_HEIGHT = 9 * multiplier;
 
 const AboutPage = () => {
   return (
-    <Layout contentStyles={{ height: "100vh" }}>
-      <h1>How to use</h1>
-      <Description>1. Add game</Description>
-      <Screenshot width={width} height={height} src={addGameImg} />
-      <Description>
-        2. Choose &quot;.exe&quot; file folder and save file location
-      </Description>
-      <Screenshot width={width} height={height} src={addGame2Img} />
-      <Description>3. Check settings</Description>
-      <Screenshot width={width} height={height} src={settingsImg} />
-      <Description>4. Now you are ready to go - run game</Description>
-      <SmallerDescription>
-        FireSave will save your game automatically only when game is running,
-        you can press F5 to save game manually(dont work in fullscreen mode for
-        now, &quot;window&quot; or &quot;borderless window&quot; only)
-      </SmallerDescription>
-      <Screenshot width={width} height={height} src={gameImg} />
+    <Layout contentStyles={{ height: "100vh", alignItems: "center" }}>
+      <Container>
+        <h1>FAQ</h1>
+
+        <FAQStyled
+          items={[
+            {
+              title: "How to add game?",
+              description: (
+                <>
+                  <Description>1. Add game</Description>
+                  <Screenshot
+                    width={SCREENSHOT_WIDTH}
+                    height={SCREENSHOT_HEIGHT}
+                    src={addGameImg}
+                  />
+                  <Description>
+                    2. Choose &quot;.exe&quot; file and save file location
+                  </Description>
+                  <Screenshot
+                    width={SCREENSHOT_WIDTH}
+                    height={SCREENSHOT_HEIGHT}
+                    src={addGame2Img}
+                  />
+                  <Description>3. Check settings</Description>
+                  <Screenshot
+                    width={SCREENSHOT_WIDTH}
+                    height={SCREENSHOT_HEIGHT}
+                    src={settingsImg}
+                  />
+                  <Description>
+                    4. Now you are ready to go - run game
+                  </Description>
+                  <SmallerDescription>
+                    FireSave will save your game automatically only when game is
+                    running, you can press F5 to save game manually(dont work in
+                    fullscreen mode for now, &quot;window&quot; or
+                    &quot;borderless window&quot; only)
+                  </SmallerDescription>
+                  <Screenshot
+                    width={SCREENSHOT_WIDTH}
+                    height={SCREENSHOT_HEIGHT}
+                    src={gameImg}
+                  />
+                </>
+              ),
+            },
+            {
+              title: "How to remove game?",
+              description: "Currently not possible",
+            },
+          ]}
+        />
+      </Container>
     </Layout>
   );
 };
+
+const Container = styled.div`
+  width: 900px;
+`;
 
 const Screenshot = styled(Image)`
   border-radius: 20px;
@@ -48,6 +90,10 @@ const Description = styled.h2`
 const SmallerDescription = styled.div`
   max-width: ${16 * multiplier}px;
   margin-bottom: 20px;
+`;
+
+const FAQStyled = styled(FAQ)`
+  margin-top: 20px;
 `;
 
 export default AboutPage;
