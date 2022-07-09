679f963d13d30cec0f73783a9cf35ad413521695
ciberusps <ciberus.ps@gmail.com>
2022-07-09 19:17:26 +0300

    publish to steam

---
+++ scripts/publishToSteam.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const exec = require("child_process").execSync;
+const path = require("path");
+
+const { checkRequiredEnvs } = require("./utils");
+
+const { STEAM_CMD, STEAM_USERNAME, STEAM_PASSWORD, WORKFOLDER } = process.env;
+checkRequiredEnvs([
+  "STEAM_CMD",
+  "STEAM_USERNAME",
+  "STEAM_PASSWORD",
+  "WORKFOLDER",
+]);
+
+const buildScriptPath = path.normalize(path.resolve("./steam_build.vdf"));
+
+try {
+  exec(
+    `${STEAM_CMD} +login ${STEAM_USERNAME} ${STEAM_PASSWORD} +run_app_build ${buildScriptPath} +quit`
+  );
+  console.info("- [publishToSteam] success");
+} catch (err) {
+  console.error(err);
+}
