9d0f1f61b786819574034b311e85bfa22f4c440e
ciberusps <ciberus.ps@gmail.com>
2022-07-10 13:56:36 +0300

    TOTP for steam guard codes

--- scripts/publishToSteam.js
+++ scripts/publishToSteam.js
@@ -4,21 +4,19 @@ const path = require("path");
 
 const { checkRequiredEnvs } = require("./utils");
 
-const { STEAM_CMD, STEAM_USERNAME, STEAM_PASSWORD, WORKFOLDER } = process.env;
+const { STEAM_CMD, STEAM_USERNAME, STEAM_PASSWORD, STEAM_GUARD_CODE } =
+  process.env;
 checkRequiredEnvs([
   "STEAM_CMD",
   "STEAM_USERNAME",
   "STEAM_PASSWORD",
-  // "WORKFOLDER",
+  "STEAM_GUARD_CODE",
 ]);
 
 const buildScriptPath = path.normalize(path.resolve("./steam_build.vdf"));
 
-// exec(`sudo ls -l ${STEAM_CMD}`, { stdio: "inherit" });
-// exec(`sudo chmod -R -f 1212 ./steamworks_sdk`, { stdio: "inherit" });
-// TODO: RUNNER_OS add sudo on macos/linux
 exec(
-  `${STEAM_CMD} +login "${STEAM_USERNAME}" "${STEAM_PASSWORD}" +run_app_build ${buildScriptPath} +quit`,
+  `${STEAM_CMD} +login "${STEAM_USERNAME}" "${STEAM_PASSWORD}" ${STEAM_GUARD_CODE} +run_app_build ${buildScriptPath} +quit`,
   { stdio: "inherit" }
 );
 console.info("- [publishToSteam] success");
